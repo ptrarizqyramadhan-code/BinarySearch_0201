77e45ffa4046e435bcca7e1a7f8ccddc64a39a77
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-06-09 22:20:10 +0700

    step8

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -64,3 +64,17 @@ void display()
     }
     cout << endl;
 }
+
+void BinarySearch()
+{
+    char ulang;
+    do
+    {
+        cout << "\n=====================================\n";
+        cout << "      Pencarian Binary Search          \n";
+        cout << ":=======================================\n";
+        cout << " Masukkan elemen yang ingin dicari: ";
+        cin >> x;
+
+        int low = 0;
+        int high = nPanjang - 1;
