4af01577ad14e68942ff5331fb1c8326a0368af0
ptrarizqyramadhan-code <ptrarizqyramadhan@gmail.com>
2026-06-09 22:16:49 +0700

    step4

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -20,3 +20,13 @@ void input()
             cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
+
+    cout << "\n====================================\n";
+    cout << "       Masukan elemen Array           \n";
+    cout << "======================================\n";
+    for (int i = 0; i <nPanjang; i++)
+    {
+        cout << "Data ke-" << (i +1) << " = ";
+        cin >> element[i];
+    }
+}
